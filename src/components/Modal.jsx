173d02b3b7e209213e932e3203550be12468d75d
곽민지 <mini0006@naver.com>
2024-01-16 10:44:27 +0900

    feat: 모달 컴포넌트 생성

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,40 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
+import React from 'react';
 import styled, { keyframes } from 'styled-components';
+import Button from './Button';
 
 const Modal = ({ type, onClose, children }) => {
-  const [portalRoot] = useState(() => document.createElement('div'));
-
-  useEffect(() => {
-    document.body.appendChild(portalRoot);
-
-    return () => {
-      document.body.removeChild(portalRoot);
-    };
-  }, [portalRoot]);
-
-  const renderModalContent = () => {
-    switch (type) {
-      case 'modal':
-        return (
-          <ModalWrapper>
-            {children}
-            <button onClick={onClose}>Close</button>
-          </ModalWrapper>
-        );
-      case 'alert':
-        return (
-          <ModalWrapper>
-            {children}
-            <button onClick={onClose}>OK</button>
-          </ModalWrapper>
-        );
-      default:
-        return null;
-    }
-  };
-
-  return createPortal(<Overlay>{renderModalContent()}</Overlay>, portalRoot);
+  const ModalWrapper = SmallWrapper;
+  return (
+    <Overlay onClick={onClose}>
+      <ModalWrapper onClick={(e) => e.stopPropagation()}>
+        <p>{children}</p>
+        <CloseButton size="small" onClick={onClose}>
+          Close
+        </CloseButton>
+      </ModalWrapper>
+    </Overlay>
+  );
 };
 
 const fadeIn = keyframes`
@@ -46,27 +25,30 @@ const fadeIn = keyframes`
   }
 `;
 
-const ModalWrapper = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: white;
-  border-radius: 8px;
-  padding: 20px;
-  animation: ${fadeIn} 0.3s ease-in-out;
-`;
-
 const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${fadeIn} 0.3s ease-in-out;
+`;
+
+const SmallWrapper = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  padding: 20px;
+  animation: ${fadeIn} 0.3s ease-in-out;
+`;
+
+const CloseButton = styled(Button)`
+  float: right;
 `;
 
 export default Modal;
